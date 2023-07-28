05a403536eac297628ef76ba4c70dbedb7275ad3
saikiranmaroju <140647354+saikiranmaroju@users.noreply.github.com>
2023-07-29 02:13:34 +0530

    Add files via upload

---
+++ face detection.py
@@ -0,0 +1,37 @@
+import cv2
+from random import randrange
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+# img = cv2.imread('nenu_bharath.jpg')
+
+webcam = cv2.VideoCapture(0)
+# key = cv2.waitKey(1)
+while True:
+    successful_frame_read, frame = webcam.read()
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    for (x,y,w,h) in face_cordinates:
+        cv2.rectangle(frame, (x,y),  (x+w, y+h), (0,256,0),2 )
+                                #(0,256,0) randrange(128,256)
+    cv2.imshow('saikiran detector ', frame)
+    key =cv2.waitKey(1)
+    if key==81 or key==113:
+        break
+
+webcam.release()   
+print("code completed")
+"""
+face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+for (x,y,w,h) in face_cordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(128,256),randrange(128,256), randrange(128,256)),2 ) 
+# [[299 307  66  66]]
+# print(face_cordinates)
+
+
+cv2.imshow('saikiran detector ', img)
+cv2.waitKey()
+print("code completed")
+"""
